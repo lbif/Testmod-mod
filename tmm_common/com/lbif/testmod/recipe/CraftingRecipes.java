fd974900041750cbda19760d466c6d58488ef06d
Asajz <hbiede@me.com>
2013-05-18 21:30:02 -0500

    Update CraftingRecipes.java

--- tmm_common/com/lbif/testmod/recipe/CraftingRecipes.java
+++ tmm_common/com/lbif/testmod/recipe/CraftingRecipes.java
@@ -2,12 +2,12 @@ package com.lbif.testmod.recipe;
 
 import com.asajz.core.handlers.recipe.RecipeHandler;
 
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class CraftingRecipes {
 	public static void initRecipes(){
-		RecipeHandler.addRecipe(new ItemStack(Item.diamond), new Object[] { "ccc", "ccc", "bbb", Character.valueOf('c'), Item.ingotIron, Character.valueOf('b'), Item.ingotGold});
+		RecipeHandler.addRecipe(new ItemStack(Item.diamond), new Object[] { "iii", "iii", "ggg", Character.valueOf('i'), Item.ingotIron, Character.valueOf('g'), Item.ingotGold});
 	}
 			
 }
