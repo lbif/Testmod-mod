6fb814e422c132b74842662b1fc2f4341661f1db
Roolean <averymuff@me.com>
2013-05-18 21:25:52 -0500

    First Recipe.

--- tmm_common/com/lbif/testmod/recipe/CraftingRecipes.java
+++ tmm_common/com/lbif/testmod/recipe/CraftingRecipes.java
@@ -1,5 +1,13 @@
 package com.lbif.testmod.recipe;
 
-public class CraftingRecipes {
+import com.asajz.core.handlers.recipe.RecipeHandler;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+
+public class CraftingRecipes {
+		public static void initRecipes(){
+			RecipeHandler.addRecipe(new ItemStack(Item.diamond, 1), new Object[] { "ccc", "ccc", "bbb", Character.valueOf('c'), Item.ingotIron, Character.valueOf('b'), Item.ingotGold});
+			}
+			
 }
